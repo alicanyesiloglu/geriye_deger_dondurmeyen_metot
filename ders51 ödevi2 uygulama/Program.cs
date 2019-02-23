c3bb2c97153bc5055c445bcd41c01c1b593069f1
alican <doc.alican@hotmail.com>
2019-02-23 14:42:22 +0300

    sinif icinde geriye deger dondurmeyen metot

---
+++ ders51 ödevi2 uygulama/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders51_ödevi2_uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pratik prt = new pratik();
+            string ad, bolum;
+            int dogum,kacgirisli;
+           
+
+            Console.Write("Adınızı girin:");
+            ad = Console.ReadLine();
+
+            Console.Write("Bolumunuz");
+            bolum = Console.ReadLine();
+
+            Console.Write("Dogum Tarihiniz:");
+            dogum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Kacgirislisiniz");
+            kacgirisli = Convert.ToInt32(Console.ReadLine());
+
+            prt.aslan(ad, bolum, kacgirisli,dogum);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ders51 ödevi2 uygulama/pratik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders51_ödevi2_uygulama
+{
+    class pratik
+    {
+       public void aslan(string ad,string bolum,int dogum,int kacgirisli)
+        {
+
+            Console.Write("adınız" + ad);
+            Console.Write("soyadınız" + bolum);
+            Console.Write("Giris tarihi" + kacgirisli);
+            Console.Write("Dogum tarihi" + dogum);
+
+
+        
+
+        }
+    }
+}
